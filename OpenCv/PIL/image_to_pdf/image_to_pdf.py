9ca3326549fb66b154f6a4fd8e61e23046c8463e
Sandeepgit <sandeep@gaia.in>
2023-08-19 23:25:46 +0530

    Multiple files changes added

--- OpenCv/PIL/image_to_pdf/image_to_pdf.py
+++ OpenCv/PIL/image_to_pdf/image_to_pdf.py
@@ -1,11 +1,29 @@
 
 import os
 from PIL import Image
+images=[]
+
+def getFiles(path):
+    for file in os.listdir(path):
+        print(file)
+        if file.endswith(".jpeg"):
+            print("in if")
+            images.append(file)
+
+
 current_root_folder= os.getcwd()
 print("Root folder name="+current_root_folder)
 project_folder="/OpenCv/PIL/image_to_pdf"
 input_folder="/inputImage/"
 output_folder="/outputFiles/"
-image_1 = Image.open(current_root_folder+project_folder+input_folder+'1.jpeg')
-im_1 = image_1.convert('RGB')
-im_1.save(current_root_folder+project_folder+output_folder+"output.pdf")
+
+image_list=[]
+getFiles(current_root_folder+project_folder+input_folder)
+
+for readImage in images:
+    image=Image.open(current_root_folder+project_folder+input_folder+readImage,"r")
+    imageObject=image.convert('RGB')
+    image_list.append(imageObject)
+
+
+image_list[0].save(current_root_folder+project_folder+output_folder+"output.pdf", save_all=True, append_images=image_list[1:])
--- OpenCv/imageResize/image_resize.py
+++ OpenCv/imageResize/image_resize.py
@@ -1,27 +1,45 @@
 import cv2
 import os
 
+images=[]
+
+def getFiles(path):
+    for file in os.listdir(path):
+        print(file)
+        if file.endswith(".jpeg"):
+            print("in if")
+            images.append(file)
+
+
 current_root_folder= os.getcwd()
 print("Root folder name="+current_root_folder)
 project_folder="/OpenCv/imageResize"
 input_folder="/inputImage/"
 output_folder="/outputImage/"
-image= cv2.imread(current_root_folder+project_folder+input_folder+"1.jpeg")
+getFiles(current_root_folder+project_folder+input_folder)
+# print("Images",images)
 
-# Get original height and width
-print(f"Original Dimensions : {image.shape}")
+for readImage in images:
 
-# resize image by specifying custom width and height
-print(image.shape[0])
-height= image.shape[0]
-width = image.shape[1]
-updatedHeight= int(height/2)
-updatedWidth= int(width/2)
+    image= cv2.imread(current_root_folder+project_folder+input_folder+readImage)
 
-#Can't parse 'dsize'. Sequence item with index 0 has a wrong type
-#solution use int casting for this
-resized = cv2.resize(image, (updatedWidth, updatedHeight ))
+    # Get original height and width
+    print(f"Original Dimensions : {image.shape}")
 
-print(f"Resized Dimensions : {resized.shape}")
-cv2.imwrite(current_root_folder+project_folder+output_folder+'resized_imaged.jpg', resized)
+    # resize image by specifying custom width and height
+    print(image.shape[0])
+    height= image.shape[0]
+    width = image.shape[1]
+    updatedHeight= int(height/3)
+    updatedWidth= int(width/3)
+
+    #Can't parse 'dsize'. Sequence item with index 0 has a wrong type
+    #solution use int casting for this
+    resized = cv2.resize(image, (updatedWidth, updatedHeight ))
+
+    print(f"Resized Dimensions : {resized.shape}")
+    cv2.imwrite(current_root_folder+project_folder+output_folder+os.path.basename(readImage), resized)
+
+
+
 
